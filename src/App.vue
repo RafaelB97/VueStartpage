3c8fe48e41dfdce7cbed7d36fe78b53684607141
Rafael Bravo <antoniorafael97@hotmail.com>
2021-01-08 12:56:20 -0600

    First wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Group v-for="group in groups" :key="group.id" :group="group"/>
+    </div>
+    <!-- {{ groups }} -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import groups from './groups'
+import Group from './components/Group'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Group,
+  },
+  data() {
+    return {
+      groups,
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: flex;
 }
 </style>
---
+++ src/components/Group.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="group">
+    <h3>{{ group.title }}</h3>
+    <ul>
+      <li v-for="bookmark in group.bookmarks" :key="bookmark" >
+        <a :href="bookmark.link">{{ bookmark.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ "group" ]
+}
+</script>
+
+<style scoped>
+.group {
+  margin: 10px;
+  background: grey;
+  padding: 10px;
+  border-radius: 15px;
+}
+
+h3 {
+  margin: 0;
+}
+</style>
